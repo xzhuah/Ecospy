0bf0194f89ef73d48f200918d30d29893bf39230
Leext <leext@buaa.edu.cn>
2016-08-16 09:16:30 +0800

    Login UI

---
+++ Android/Login/app/src/test/java/team9/login/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package team9.login;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
