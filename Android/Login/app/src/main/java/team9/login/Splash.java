1e026e98720e7b44be6b72f4967f10cc3e7345d2
Leext <leext@buaa.edu.cn>
2016-08-16 12:03:09 +0800

    add loading picture

---
+++ Android/Login/app/src/main/java/team9/login/Splash.java
@@ -0,0 +1,27 @@
+package team9.login;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity
+{
+    private static final int LOAD_DISPLAY_TIME = 1500;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable()
+        {
+            public void run()
+            {
+                          /* Create an Intent that will start the Main WordPress Activity. */
+                Intent mainIntent = new Intent(Splash.this, LoginActivity.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, LOAD_DISPLAY_TIME); //1500 for release
+    }
+}
